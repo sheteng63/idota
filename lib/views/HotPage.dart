cd2421dc011a32af15ba8a791ddf1775e0d0f5fb
sheteng <sheteng@pi2e.com>
2018-06-04 10:13:31 +0800

    修改启动也

--- lib/views/HotPage.dart
+++ lib/views/HotPage.dart
@@ -6,63 +6,63 @@ class HotPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new ListView(
       children: <Widget>[
-        new Container(
-          margin: const EdgeInsets.all(5.0),
-          padding:const EdgeInsets.all(5.0) ,
-          height: 150.0,
-          decoration: new BoxDecoration(
-              image: new DecorationImage(
-                  image: new NetworkImage(
-                    "http://c.hiphotos.baidu.com/image/h%3D300/sign=e3b76ad36081800a71e58f0e813433d6/d50735fae6cd7b89acbea9df032442a7d8330e9f.jpg",
-                  ),
-                  fit: BoxFit.cover)),
-          child: new Row(
-            children: <Widget>[
-              new Flexible(
-                flex: 4,
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    new Flexible(
-                      child: new Text(
-                        "NATIVEONDRAW",
-                        style: new TextStyle(
-                            fontSize: 18.0,
-                            color: Theme.of(context).primaryColor),
-                      ),
-                      flex: 1,
+        new GestureDetector(
+          child: new Container(
+            margin: const EdgeInsets.all(5.0),
+            padding: const EdgeInsets.all(5.0),
+            height: 150.0,
+            decoration: new BoxDecoration(
+                image: new DecorationImage(
+                    image: new NetworkImage(
+                      "http://c.hiphotos.baidu.com/image/h%3D300/sign=e3b76ad36081800a71e58f0e813433d6/d50735fae6cd7b89acbea9df032442a7d8330e9f.jpg",
                     ),
-                    new Flexible(
-                      flex: 1,
-                      child: new Text(
-                        "nativeOnDraw failed; clearing to background color",
-                        style: new TextStyle(fontSize: 15.0),
+                    fit: BoxFit.cover)),
+            child: new Row(
+              children: <Widget>[
+                new Flexible(
+                  flex: 4,
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      new Flexible(
+                        child: new Text(
+                          "NATIVEONDRAW",
+                          style: new TextStyle(
+                              fontSize: 18.0,
+                              color: Theme.of(context).primaryColor),
+                        ),
+                        flex: 1,
                       ),
-                    )
-                  ],
+                      new Flexible(
+                        flex: 1,
+                        child: new Text(
+                          "nativeOnDraw failed; clearing to background color",
+                          style: new TextStyle(fontSize: 15.0),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              new Flexible(
-                flex: 1,
-                child: new IconButton(
-                  icon: new Icon(
+                new Flexible(
+                  flex: 1,
+                  child: new Icon(
                     Icons.arrow_forward,
                     color: Theme.of(context).primaryColor,
                   ),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        new MaterialPageRoute(builder: (BuildContext context) {
-                      return new WebViewPage(
-                        url: "http://www.baidu.com",
-
-                      );
-                    }));
-                  },
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+          onTap: () {
+            Navigator
+                .of(context)
+                .push(new MaterialPageRoute(builder: (BuildContext context) {
+              return new WebViewPage(
+                url: "http://www.wanmei.com",
+              );
+            }));
+          },
+        )
       ],
     );
   }
